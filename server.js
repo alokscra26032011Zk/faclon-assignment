8b16b96fea22b7afbc05e5659e2dfe46d588e931
aloscra26032011Zk <alokscra26032011@gmail.com>
2016-09-30 12:37:30 +0530

    third commit

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var express=require('express');
 var app=express();
 var server=require('http').createServer(app);
-var io=require('socket.io').listen(server);
+var io=require('socket.io');
 
 
 server.listen(process.env.PORT || 3000);
 console.log('Server running...')
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/public/index.html')
+})
+
+
+var listener = io.listen(server);
+listener.sockets.on('connection', function(socket){
+    socket.emit('numberTime', [{'number':23,'time':1},{'number':34,'time':5},{'number':88,'time':3},{'number':90,'time':2}]);
 })
